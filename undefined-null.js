9b1480cc19d017cfae7d55326aa5ccba48166e52
Tugay <tugayjabbarov@gmail.com>
2025-02-10 12:31:42 +0400

    undefined-null.js file added

---
+++ undefined-null.js
@@ -0,0 +1,15 @@
+// Special values 
+
+
+// default value 
+
+// const name;  name = undefined
+
+
+const myarray = [];
+// null ilə undefined arasında fərq var: undefined JavaScript-də bir dəyişənin
+//  heç təyin edilmədiyini göstərir, 
+// amma null təyin edilmiş, 
+// lakin boş olan bir dəyişəndir.
+
+// NaN ==> 'hell0' -7   value = NaN
