9fc950f28bd9d15dcc0cc5405874c36bf63aabb9
Tugay <tugayjabbarov@gmail.com>
2025-02-10 13:34:23 +0400

    main.js file try added

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log("Hello")
+
+console.log(typeof(2));
+console.log(typeof("hello")); 
+console.log(typeof true);
+console.log(typeof(undefined)); 
+console.log(typeof{name:'Rasul',age:17});
+console.log(typeof[1,21,12]);
