272bc3225312f099f59733377a89fdc39f59dabb
Tugay <tugayjabbarov@gmail.com>
2025-02-10 20:25:30 +0400

    operators.js file added

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-console.log("Hello")
+// // let name = "Tugay";
+// // let surname = "Jabbarov";
+// // let fullName = "Hello " + name + " " + surname;
 
-console.log(typeof(2));  ==> number
-console.log(typeof("hello")); ==> string
-console.log(typeof true);  ==> boolean
-console.log(typeof(undefined)); ==> undefined
-console.log(typeof{name:'Rasul',age:17}); ==> object
-console.log(typeof[1,21,12]); ==> object
+// <<<<<<< HEAD
+// console.log(typeof(2));  ==> number
+// console.log(typeof("hello")); ==> string
+// console.log(typeof true);  ==> boolean
+// console.log(typeof(undefined)); ==> undefined
+// console.log(typeof{name:'Rasul',age:17}); ==> object
+// console.log(typeof[1,21,12]); ==> object
+// =======
+// // console.log(fullName);
+
+
+// // let escapeExample = "He said,\"JS is cooll\ ";
+
+
+// // Template Literal
+// // let name = "Tugay";
+// // let message = `Hello, my name is ${name}`;
+// // console.log(message)
+
+
+// // Multi line strings
+
+// let multilineString  = `My name is
+//         Tugay`
+
+// console.log(multilineString)
+// >>>>>>> c3912a2 (operators.js file added)
---
+++ operators.js
@@ -0,0 +1,29 @@
+let n1 = 10;
+let n2 = 7;
+
+// console.log(n1 === n2) False  Not-Equality
+// console.log(n1 !== n2) True  Equality
+
+console.log(n1 + n2)
+console.log(n1 - n2)
+console.log(n1 * n2)
+console.log(n1 / n2)
+console.log(n1 % n2)
+
+
+
+console.log('Hello ' + 'world!')
+
+
+let x = 5;
+let y = ++x;
+
+console.log(x);
+console.log(y)
+
+
+let a = 5;
+let b = a ++;
+
+console.log(a);
+console.log(b);
