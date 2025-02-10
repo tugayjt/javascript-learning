63ab4d71a62ea3570a46fa2d139d4b8cf4c28a5a
Tugay <tugayjabbarov@gmail.com>
2025-02-10 09:30:01 +0400

    Variable

--- variables.js
+++ variables.js
@@ -1,1 +1,14 @@
-let a = "5"
+// let myVariable = "Test";
+
+
+
+// let myVariable;
+// myVariable = "Test";
+
+
+// const PI = 3.14;
+// console.log(PI);
+
+// PI = 3.2123;
+// Assignment to constant varibale
+
