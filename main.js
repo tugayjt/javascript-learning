dc2abf1ecedbad3077131d077f470d24af8e4ffa
tugayjt <142034909+tugayjt@users.noreply.github.com>
2025-02-10 13:36:23 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 console.log("Hello")
 
-console.log(typeof(2));
-console.log(typeof("hello")); 
-console.log(typeof true);
-console.log(typeof(undefined)); 
-console.log(typeof{name:'Rasul',age:17});
-console.log(typeof[1,21,12]);
+console.log(typeof(2));  ==> number
+console.log(typeof("hello")); ==> string
+console.log(typeof true);  ==> boolean
+console.log(typeof(undefined)); ==> undefined
+console.log(typeof{name:'Rasul',age:17}); ==> object
+console.log(typeof[1,21,12]); ==> object
