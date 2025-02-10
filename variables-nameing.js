3291396922a231183ad280cf4684b86266528c59
Tugay <tugayjabbarov@gmail.com>
2025-02-10 12:19:44 +0400

    variables-nameing.js file added

---
+++ variables-nameing.js
@@ -0,0 +1,24 @@
+// Naming rules Allowed
+
+// let userName ==> (cames case)
+
+// let ageGroup5 Only Letters and Digits
+
+// let $price Starting with $ is allowed
+
+// let_value Startin with _ is allowed
+
+
+
+// NOT ALLOWED
+
+
+// let user_names ==> bad practise
+
+
+// let 3friends  Starting Digits not allowed
+
+// let az-en     Special characters is not allowed
+
+// let let Reseved Keywords is not allowed
+
