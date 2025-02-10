b2552d0b13b7c349553fc75b6e5f379c3ff485ef
Tugay <tugayjabbarov@gmail.com>
2025-02-10 10:01:32 +0400

    data-types.js file added

---
+++ data-types.js
@@ -0,0 +1,22 @@
+// string
+// let name = "Tugay";
+// let surname = "Jabbarov";
+// let country = 'Azerbaijan';
+
+// number
+let point = 20;
+let minpoint = -1;
+letmaxpoint = 1;
+
+// boolean
+let isMale = true;
+let isPlayer = false;
+
+
+
+// let countries = ['Az','Tr','Uk']; string array
+// let degrees = [5,8,12,56]; number array
+// let answers = [true,false,]; boolean array
+
+
+let person = { name: "Tugay", age: 20, surname: "Jabbarov" }
