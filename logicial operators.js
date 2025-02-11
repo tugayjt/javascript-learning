9c8729d379f03687fe788b78492f57a56f8ea5a6
Tugay <tugayjabbarov@gmail.com>
2025-02-11 15:50:18 +0400

    logicial operators.js file added

---
+++ logicial operators.js
@@ -0,0 +1,12 @@
+const userInput = 0; "Folsy"
+
+const value1 = userInput || "Baku" || "Istanbul";
+
+const value2 = userInput && "Nervous";
+
+const value3 = !!userInput
+
+
+console.log(value1); "Baku"
+console.log(value2);  "0"
+console.log(value3);  ""
