fe5ca82c863c42f1d26f52da8587280fb3466f97
Tugay <tugayjabbarov@gmail.com>
2025-02-11 18:24:01 +0400

    rest.js file added

---
+++ rest.js
@@ -0,0 +1,8 @@
+function getNumbers(a,b, ...rest){
+    console.log("First Number:",a); "1"
+    console.log("First Number:",b); "2"
+    console.log("Rest of the numbers:",rest)  |"[3,4,5,6,7,8,9,10]"
+}
+
+
+getNumbers(1,2,3,4,5,6,7,8,9,10)
