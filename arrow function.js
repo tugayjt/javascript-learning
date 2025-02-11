20ba2c162fc71c4ef70628eef669fa2731a563f9
Tugay <tugayjabbarov@gmail.com>
2025-02-11 18:16:23 +0400

    arrow function.js file added

---
+++ arrow function.js
@@ -0,0 +1,10 @@
+const sum = (a,b) => (a +b);
+console.log(sum(5,6));
+
+
+const greet = () => "Hello!";
+console.log(greet()); // Hello!
+
+
+const square = num => num * num;
+console.log(square(4)); // 16
