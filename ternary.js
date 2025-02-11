6b7624126db366c64b35ef6060fdc9a093780e4f
Tugay <tugayjabbarov@gmail.com>
2025-02-11 15:11:14 +0400

    ternary.js file added

---
+++ ternary.js
@@ -0,0 +1,13 @@
+let age = 18;
+
+let message = age >= 18 ? "You are an adult" : "You are a minor"
+
+console.log(message)
+
+
+
+let price = 150;
+
+let notfication = price >= 50 ? "It is expensive" : "It is a cheap"
+
+console.log(notfication)
