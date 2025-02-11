99e2dbbb87f98ad7e0ae2392041640bced80b799
Tugay <tugayjabbarov@gmail.com>
2025-02-11 14:52:10 +0400

    comparing.js file added

---
+++ comparing.js
@@ -0,0 +1,33 @@
+const person1 = { name: "Alex" }
+const person2 = { name: "Alex" }
+
+
+const n1 = 6;
+const n2 = 6;
+
+console.log(n1 === n2); "TRUE"
+console.log(n1 == n2); "TRUE"
+
+console.log(person1 === person2); "FALSE"
+console.log(person1 == person2); "FALSE"
+
+console.log(person1.name === person2.name); "TRUE"
+console.log(person1.name == person2.name); "TRUE"
+
+// const person1 = {name:"Alex"}
+// const person2 = {name:"Alex"}
+// const person3 = person2
+
+// const n1 = 6;
+// const n2 = 6;
+
+// console.log(n1 === n2); "TRUE"
+// console.log(n1 == n2);  "TRUE"
+
+// console.log(person1 === person2);  "FALSE"
+// console.log(person1 == person2);   "FALSE"
+// console.log(person3 == person2);   "TRUE"
+
+
+// console.log(person1.name === person2.name); "TRUE"
+// console.log(person1.name == person2.name);  "TRUE"
