03fb604e100a6093b935f83b1f4482211a07a23f
Tugay <tugayjabbarov@gmail.com>
2025-02-12 15:45:59 +0400

    splice.js file added

---
+++ array-methods.js
@@ -0,0 +1,9 @@
+const arr = ["python", "c++"];
+
+arr.push("java");
+
+arr.pop();
+
+arr.shift();
+
+arr.unshift();
---
+++ splice.js
@@ -0,0 +1,18 @@
+const arr = ["Mexico","Italy","Spanish","Portuges"];
+
+const arr2 = ['Salman','Telman',"Suleyman","Rehman"];
+
+const arr3 = [1,2,3,6,9,12];
+
+const arr4 = ["JS","Python","C#"];
+
+
+arr.splice(1);
+
+arr2.splice(1,2);
+
+arr3.splice(1,4,"Tugay","Rauf");
+
+arr4.splice(-2,1);
+
+console.log(arr3);
