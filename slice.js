c0b65435490850f3eeec26d574a9827ecd82d510
Tugay <tugayjabbarov@gmail.com>
2025-02-12 15:57:23 +0400

    slice.js file added

---
+++ slice.js
@@ -0,0 +1,12 @@
+const arr1 = ["Mexico", "Italy", "Spanish", "Portuges"];
+
+const arr2 = arr1;
+
+
+
+const arr3 = arr1.slice();
+const arr4 = arr3.slice(1);
+
+
+console.log(arr3);
+console.log(arr4);
