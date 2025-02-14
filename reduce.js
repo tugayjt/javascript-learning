f4d4d0a0fd4a468a83ec865349f50e1c14256e51
Tugay <tugayjabbarov@gmail.com>
2025-02-14 10:18:19 +0400

    reduce.js file added

--- reduce.js
+++ reduce.js
@@ -1,6 +1,5 @@
-let x = [1, 2, 3, 4, 5];
-let s = x.reduce((x, s) => x + s, 0);
-// console.log(s)
+
+
 
 
 let ededler = [1, 2, 2, 3, 4, 4, 5, 8, 92];
@@ -8,15 +7,13 @@ let ededler2 = [1, 2, 2, 3, 4, 1, 4, 5, 6, 32];
 let a = []
 let b = {}
 for (let i = 0; i < ededler.length; i++) {
-    for (let j = 0; j < ededler2.length; j++) {
+    for (let j = i + 1; j < ededler2.length; j++) {
         if(ededler[i] === ededler2[j]){
-            break;
+            continue;
         }
         else{
-            a.push(ededler[i]);
-            break;
+            a.push(ededler[i])
         }
     }
 }
-console.log(a)
-
+console.log(a)        
