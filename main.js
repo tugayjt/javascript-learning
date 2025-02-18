ff5757ce924b1ef2666d9d250ae452cc0113a1d1
Tugay <tugayjabbarov@gmail.com>
2025-02-18 10:45:26 +0400

    objects.js file added

--- main.js
+++ main.js
@@ -1,32 +1,20 @@
-let num = 123;
-let str = String(num);  // "123"
-console.log(typeof str); // "string"
+const person = {
+    name:"Arif",
+    surname:"Aliyev",
+    age: 18,
+    language: ['AZ','ENG',"RU"],
+    sayHi: function(){
+        console.log("Hello");
+    },
+};
 
 
-
-let string = num.toString(); // "123"
-console.log(typeof string); // "string"
-
-
-let str2 = "456";
-let num2 = Number(str); // 456
-console.log(typeof num); // "number"
+person.age;
+person.name;
 
 
-let bool1 = Boolean(1); // true
-let bool2 = Boolean(0); // false
-let bool3 = Boolean(""); // false
-let bool4 = Boolean("Hello"); // true
+person["sayHi"];
 
+person.language.push("Italian");
 
-let t = "100px";
-let n = parseInt(t); // 100
-console.log(n); // 100
-
-
-let jsonString = '{"name": "Ali", "age": 25}';
-let obj = JSON.parse(jsonString);
-console.log(obj.name); // "Ali"
-console.log(obj.age); // 25
-
-
+person.sayHi();
