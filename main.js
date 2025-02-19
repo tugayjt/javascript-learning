466d22d9fe69e7bb296f5a7db8e4f5c34229328d
Tugay <tugayjabbarov@gmail.com>
2025-02-19 11:49:06 +0400

    classes.js file added

---
+++ main.js
@@ -0,0 +1,14 @@
+class Car{
+    make;
+    model;
+    year;
+
+        constructor(make,model,year){
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+}
+
+
+const tesla = new Car("Tesla","Model_3","")
