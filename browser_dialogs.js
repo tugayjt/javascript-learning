f935e4a3a76d9be856047fc550ea17a87bf58b2b
Tugay <tugayjabbarov@gmail.com>
2025-02-22 22:54:07 +0400

    browser_dialogs.js file added

---
+++ browser_dialogs.js
@@ -0,0 +1,15 @@
+alert("Hello World");
+window.alert("Hello")
+
+
+let name = prompt("Enter your name: ");
+alert("Hello, " + name + "!")
+
+
+let remove = confirm("Are you sure remove this file?");
+if (remove){
+    alert("File removed")
+} else{
+    alert("The operation was canceled.")
+}
+
