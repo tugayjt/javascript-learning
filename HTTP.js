e88cc98aef9cb4ba013a55019ec3f178ea85984d
Tugay <tugayjabbarov@gmail.com>
2025-02-27 10:34:57 +0400

    HTTP.js file re-added

--- HTTP.js
+++ HTTP.js
@@ -15,7 +15,7 @@ fetch('https://api.github.com/users/iliakan', {
     method: 'POST',
     headers: {  'Content-Type': 'application/json' },
     body: JSON.stringify({
-    name: 'John',               
+    name: 'John',             
     age: 30,
     isAdmin: true
     })
@@ -32,3 +32,28 @@ fetch('https://api.github.com/users/iliakan', {
     })
     })
     .then(() => console.log('Post updated.'));  
+
+
+
+
+
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: "DELETE",
+    headers: {
+        'Content': 'appliction'
+    },
+    body:JSON.stringify({title: 'New POST', body: 'New context',userid: 1})
+    })
+
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error))
+
+
+
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'DELETE'
+      })
+        .then(() => console.log('Post silindi'))
+        .catch(error => console.error('Xəta:', error));
+      
