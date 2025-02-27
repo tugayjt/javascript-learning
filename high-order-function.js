51e49d0aceef27e392e0fd6e33708e829a95d13c
Tugay <tugayjabbarov@gmail.com>
2025-02-27 22:44:50 +0400

    high-order-function.js file added

---
+++ high-order-function.js
@@ -0,0 +1,25 @@
+function process(callback) {
+    console.log("İşlənir...");
+    callback(); 
+}
+
+function sayHello() {
+    console.log("Salam!");
+}
+
+process(sayHello);
+
+
+
+function multiplier(factor) {
+    return function(number) {
+        return number * factor;
+    };
+}
+
+const double = multiplier(2);
+console.log(double(5)); 
+
+const triple = multiplier(3);
+console.log(triple(5)); 
+
