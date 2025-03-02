bd68976192d46cdda81caa71393a1a52316370f5
Tugay <tugayjabbarov@gmail.com>
2025-03-02 14:57:14 +0400

    callstack.js file added

---
+++ callstack.js
@@ -0,0 +1,34 @@
+function first(){
+    console.log("First");
+    second();
+    console.log("first ended");
+}
+
+
+function second(){
+    console.log("Second");
+    third();
+    console.log("Second ended");
+}
+
+function third(){
+    console.log("third")
+}
+
+first();
+
+
+const f1 = () => "1"
+
+const f2 = () => `${f1()} ---> 2`
+
+const f3 = () => `${f2()} ---> 3`
+
+f3()
+
+
+function recursion() {
+    recursion(); 
+}
+
+recursion(); 
