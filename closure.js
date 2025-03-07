48259fe7d57e8a418de892e0a1b45af847ae9534
Tugay <tugayjabbarov@gmail.com>
2025-03-07 17:36:35 +0400

    closure.js file added

--- closure.js
+++ closure.js
@@ -8,6 +8,33 @@ function outerFunction() {
     return innerFunction;  
 }
 
+const closureExtample = outerFunction(); 
+closureExample(); 
+
+
+function outerFunction() {
+    let outerVar = 'xarici dəyişən';
+    
+    function innerFunction() {
+        console.log(outerVar);   
+    }
+    
+    return innerFunction;
+}
+
+const closureFunchtion = outerFunction();
+closureFunction();  
+
+function outerFunction() {
+    let outerVar = "Mən xarici dəyişənəm";
+
+    function innerFunction() {
+        console.log(outerVar); 
+    }
+
+    return innerFunction;  
+}
+
 const closureExample = outerFunction(); 
 closureExample(); 
 
