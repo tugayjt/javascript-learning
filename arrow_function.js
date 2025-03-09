4556995be093c1fb39ab243e0c7a48d432f1e705
Tugay <tugayjabbarov@gmail.com>
2025-03-09 23:42:58 +0400

    arrow_function.js  file added

--- arrow_function.js
+++ arrow_function.js
@@ -8,3 +8,16 @@ console.log(greet()); // Hello!
 
 const square = num => num * num;
 console.log(square(4)); // 16
+
+
+
+const sgum = (a,b) => (a +b);
+console.log(sum(5,6));
+
+
+const gretet = () => "Hello!";
+console.log(greet()); // Hello!
+
+
+const squyare = num => num * num;
+console.log(square(4)); // 16
